a43aa4ac3e462398f8fdc6c0c8810f4d93dc6fc1
Alex Muller <mullerbox@gmail.com>
2018-10-13 15:19:14 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+require('./bootstrap')
+
+window.Vue = require('vue')
+
+import App from './components/App'
+import axios from 'axios'
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('app', App)
+
+const app = new Vue({
+  el: '#app'
+})
---
+++ resources/js/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+<b-container>
+  <b-row>
+    <b-col>
+      <b-jumbotron header="Shutterstock Image Sender" lead="Don't use for spam :)" >
+      </b-jumbotron>
+    </b-col>
+  </b-row>
+  <b-row>
+    <b-col>
+      <b-form-group>
+        <b-form-input
+          type="text"
+          placeholder="Search images"></b-form-input>
+      </b-form-group>
+      <b-form-group>
+        <b-form-file v-model="file" :state="Boolean(file)" placeholder="Choose a file..."></b-form-file>
+      </b-form-group>
+    </b-col>
+  </b-row>
+  <b-row class="justify-content-center">
+    <b-col lg="3" md="3" sm="3" xs="6" v-for="n in 20" :key="n" class="mb-2">
+      <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Thumbnail" />
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+
+</script>
